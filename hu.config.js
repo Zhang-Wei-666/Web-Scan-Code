ac885a1be85bb4f5234217aea0349e2628e64bb2
wei.zhang <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-08-17 19:11:06 +0800

    Update hu.config.js

--- hu.config.js
+++ hu.config.js
@@ -3,7 +3,16 @@ const HU_RUNNING_COMMAND = process.env.HU_RUNNING_COMMAND;
 
 module.exports = {
   mode: HU_RUNNING_COMMAND === 'build',
+  format: 'esm',
+
+  browserslist: [
+    'last 2 Chrome versions'
+  ],
+
   replace: {
     'process.env.NODE_ENV': JSON.stringify(HU_RUNNING_COMMAND === 'build' ? 'production' : 'development')
+  },
+  bundleReplace: {
+    'process.env.html': ''
   }
 };
