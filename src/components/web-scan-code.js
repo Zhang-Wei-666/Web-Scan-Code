2f4ffec3dba27cf46e74c60c2950f86657ae4c13
wei.zhang <37876877+Zhang-Wei-666@users.noreply.github.com>
2020-08-18 16:26:47 +0800

    优化扫码界面样式

--- src/components/web-scan-code.js
+++ src/components/web-scan-code.js
@@ -1,28 +1,53 @@
 import elementStyle from './web-scan-code.scss?toString';
 
 
-const elementTemplate = document.createElement('template');
-
-elementTemplate.innerHTML = process.env.html`
-  <style>${elementStyle}</style>
-  <div>
-    <video ref="video" playsinline autoplay></video>
-  </div>
-`;
-
 class WebScanCode extends HTMLElement {
   constructor() {
     super();
 
-    const shadowRoot = this.$el = this.attachShadow({ mode: 'open' });
-    const refs = this.$refs = {};
+    this.$el = this.attachShadow({ mode: 'open' });
+    this.$refs = {};
 
-    shadowRoot.appendChild(
+    this.render();
+  }
+
+  /**
+   * 渲染元素内容
+   */
+  render() {
+    const { $el, $refs } = this;
+    const elementTemplate = document.createElement('template');
+
+    // 安装 VSCode 的 lit-html 插件就可以高亮
+    elementTemplate.innerHTML = process.env.html`
+      <style>${elementStyle}</style>
+      <div id="app">
+        <!-- 标题区域 -->
+        <div id="header">
+          <!-- 关闭按钮 -->
+          <svg class="close" viewBox="0 0 1024 1024">
+            <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8c-14.7 12.8-14.7 35.6 0 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"></path>
+          </svg>
+          <!-- 竖线 -->
+          <div class="gap"></div>
+          <!-- 标题 -->
+          <div class="title">扫一扫</div>
+        </div>
+        <div id="content">
+          <!-- 视频流显示 -->
+          <video ref="video" playsinline autoplay></video>
+        </div>
+      </div>
+    `;
+
+    // 写入元素内容
+    $el.appendChild(
       elementTemplate.content.cloneNode(true)
     );
 
-    Array.from(shadowRoot.querySelectorAll('[ref]')).forEach((elem) => {
-      refs[elem.getAttribute('ref')] = elem;
+    // 查找所有注册的引用信息
+    Array.from($el.querySelectorAll('[ref]')).forEach((elem) => {
+      $refs[elem.getAttribute('ref')] = elem;
     });
   }
 
--- src/index.js
+++ src/index.js
@@ -19,22 +19,28 @@ export default async function webScanCode(options = {}) {
   }
 
   // 获取当前设备摄像头 ID 数组
+  // 执行这一步会询问摄像头权限
   videoDevices = videoDevices || (await navigator.mediaDevices.enumerateDevices()).filter((device) => {
     return device.kind === 'videoinput';
   });
 
-  /** 视频流 */
+  /** @type {Element} 显示扫码界面 */
+  const elem = document.body.appendChild(document.createElement('web-scan-code'));
+  /** @type {Element} 显示视频流的 video 标签 */
+  const video = elem.$refs.video;
+  /** @type {DOMRect} 显示视频流的 video 标签元素位置 */
+  const videoRect = video.getBoundingClientRect();
+
+  /** @type {MediaStream} 视频流 */
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: {
+      width: videoRect.height,
+      height: videoRect.width,
       deviceId: { exact: videoDevices[6].deviceId }
     }
   });
-  /** 显示扫码界面 */
-  const elem = document.body.appendChild(
-    document.createElement('web-scan-code')
-  );
 
-
-  elem.$refs.video.srcObject = stream;
+  // 播放视频流
+  video.srcObject = stream;
 }
